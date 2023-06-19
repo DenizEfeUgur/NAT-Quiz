4a3d4609073f96aed0fa3c7ff010befdaf29b732
Deniz Ugur <deniz.ugur2006@hotmail.com>
2023-06-19 11:03:02 +0200

    Flaggen Questions done

---
+++ nat-quiz/src/Flaggen.js
@@ -0,0 +1,113 @@
+/** @format */
+
+import React from "react";
+import "./flagge.css";
+
+const Flaggen = () => {
+  const flaggen = [
+    {
+      flag: "https://cdn.pixabay.com/photo/2012/04/10/23/11/switzerland-26891_1280.png",
+      options: ["Canada", "Russland", "USA", "Schweiz"],
+      answer: "Schweiz",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2015/11/12/16/23/flag-1040575_1280.png",
+      options: ["Neuseeland", "Zypern", "Columbien", "England"],
+      answer: "Zypern",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2013/07/13/14/15/italy-162326_1280.png",
+      options: ["Kroatien", "Italien", "Thailand", "Indien"],
+      answer: "Italien",
+    },
+
+    {
+      flag: "https://cdn.pixabay.com/photo/2012/04/12/23/52/germany-31017_1280.png",
+      options: ["Deutschland", "Frankreich", "Spanien", "Italien"],
+      answer: "Deutschland",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2013/07/12/17/58/thailand-152711_1280.png",
+      options: ["Costa Rica", "Thailand", "Lettland", "Estland"],
+      answer: "Thailand",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2013/07/13/14/16/maldives-162352_1280.png",
+      options: ["Türkei", "Algerien", "Malediven", "Libanon"],
+      answer: "Malediven",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2013/07/13/14/16/new-zealand-162373_1280.png",
+      options: ["Neuseeland", "Australien", "cookinseln", "Belgien"],
+      answer: "Neuseeland",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2013/07/13/14/16/mozambique-162366_1280.png",
+      options: ["Mosambik", "Belarus", "Kenia", "Nicaragua"],
+      answer: "Mosambik",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2017/03/14/21/00/american-flag-2144392_1280.png",
+      options: ["Kanada", "USA", "Mexiko", "Brasilien"],
+      answer: "USA",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2013/07/13/14/16/mexico-162359_1280.png",
+      options: ["Kanada", "USA", "Mexiko", "Brasilien"],
+      answer: "Mexiko",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2012/04/10/23/17/ethiopia-26943_1280.png",
+      options: ["Ghana", "Mali", "Tansania", "Äthiopien"],
+      answer: "Äthiopien",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2016/10/27/12/55/turkish-flag-1774834_1280.png",
+      options: ["Algerien", "Türkei", "Marokko", "Tunesien"],
+      answer: "Türkei",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2015/10/24/21/30/albania-1005017_1280.png",
+      options: ["Bosnien", "Albanien", "Serbien", "Mazedonien"],
+      answer: "Albanien",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2013/07/13/14/17/swaziland-162432_1280.png",
+      options: ["Benin", "Gabun", "Swasiland", "Mauritius"],
+      answer: "swasiland",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2013/07/13/14/16/lesotho-162340_1280.png",
+      options: ["Lesotho", "Mauretanien", "Seychellen", "Malawi"],
+      answer: "Lesotho",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2013/07/13/14/17/south-africa-162425_1280.png",
+      options: ["Südafrika", "Kenia", "Äthiopien", "Nigeria"],
+      answer: "Südafrika",
+    },
+    {
+      flag: " https://cdn.pixabay.com/photo/2013/07/13/14/15/fiji-162293_1280.png",
+      options: ["Burundi", "Botsuana", "Fidschi", "Gambia"],
+      answer: "Fidschi",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2013/07/13/14/16/monaco-162361_1280.png",
+      options: ["Indonesien", "Monaco", "Polen", "Tunesien"],
+      answer: "Monaco",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2013/07/13/14/17/uruguay-162455_1280.png",
+      options: ["Uruguay", "Paraguay", "Griechenland", "Argentinien"],
+      answer: "Uruguay",
+    },
+    {
+      flag: "https://cdn.pixabay.com/photo/2020/12/05/06/40/kazakhstan-5805211_1280.png",
+      options: ["Irland", "Norwegen", "Kasachstan", "Slowakei"],
+      answer: "Kasachstan",
+    },
+  ];
+  return null;
+};
+
+export default Flaggen;
