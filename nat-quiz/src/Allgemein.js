0d7744334a2f9bcf42b100bd890da0cede428a19
Deniz Ugur <deniz.ugur2006@hotmail.com>
2023-06-19 10:53:25 +0200

    Allgemein questions done

--- nat-quiz/src/Allgemein.js
+++ nat-quiz/src/Allgemein.js
@@ -1,1 +1,150 @@
 /** @format */
+
+import React from "react";
+import "./allgemein.css";
+
+const Allgemein = () => {
+  const questions = [
+    {
+      question: "Welches ist das grösste Land der Welt?",
+      bild: "https://cdn.pixabay.com/photo/2016/04/01/22/32/world-1301744_1280.png",
+      options: ["Canada", "Russland", "USA", "China"],
+      answer: "Russland",
+    },
+    {
+      question: "In welchem Jahr endete der Zweite Weltkrieg?",
+      bild: "https://cdn.pixabay.com/photo/2017/06/10/06/39/calender-2389150_1280.png",
+      options: ["1939", "1944", "1945", "1950"],
+      answer: "1945",
+    },
+    {
+      question: "Wie viele Planeten hat unser Sonnensystem?",
+      bild: "https://cdn.pixabay.com/photo/2012/04/10/17/38/saturn-26618_1280.png",
+      options: ["7", "8", "9", "10"],
+      answer: "8",
+    },
+    {
+      question: "Welches ist der längste Fluss der Welt?",
+      bild: "https://cdn.pixabay.com/photo/2015/08/26/17/35/water-908813_1280.png",
+      options: ["Nil", "Amazonas", "Mississippi", "Jangtse"],
+      answer: "Nil",
+    },
+    {
+      question: "Was ist die chemische Formel für Sauerstoff?",
+      bild: "https://cdn.pixabay.com/photo/2013/07/13/01/07/chemistry-155121_1280.png",
+      options: ["H2O", "CO2", "O2", "NaCl"],
+      answer: "O2",
+    },
+    {
+      question: "Welches ist das höchste Gebirge der Welt?",
+      bild: "https://cdn.pixabay.com/photo/2012/04/13/01/09/mountains-31590_1280.png",
+      options: ["Anden", "Himalaya", "Rocky Mountains", "Alpen"],
+      answer: "Himalaya",
+    },
+    {
+      question: "Wie viele Zähne hat ein Mensch?",
+      bild: "https://cdn.pixabay.com/photo/2013/07/13/11/28/dentist-158225_1280.png",
+      options: ["24", "26", "32", "36"],
+      answer: "32",
+    },
+    {
+      question: "Wer ist der Gründer von Microsoft?",
+      bild: "https://cdn.pixabay.com/photo/2012/04/10/23/33/logo-27046_1280.png",
+      options: ["Bill Gates", "Steve Jobs", "Mark Zuckerberg", "Jeff Bezos"],
+      answer: "Bill Gates",
+    },
+    {
+      question: "Was ist die Hauptstadt von Australien?",
+      bild: "https://cdn.pixabay.com/photo/2016/03/31/21/55/australia-1296727_1280.png",
+      options: ["Sydney", "Melbourne", "Canberra", "Perth"],
+      answer: "Canberra",
+    },
+    {
+      question: "Wie viele Ozeane gibt es auf der Erde?",
+      bild: "https://cdn.pixabay.com/photo/2014/04/02/11/01/wave-305226_1280.png",
+      options: ["3", "4", "5", "6"],
+      answer: "5",
+    },
+    {
+      question: "Wie viele Knochen hat ein Mensch?",
+      bild: "https://cdn.pixabay.com/photo/2013/07/13/10/27/bone-157272_1280.png",
+      options: ["187", "156", "127", "206"],
+      answer: "206",
+    },
+    {
+      question: "Wer malte das Gemälde 'Die Mona Lisa'?",
+      bild: "https://cdn.pixabay.com/photo/2016/03/19/04/40/paint-1266212_1280.png",
+      options: [
+        "Vincent van Gogh",
+        "Pablo Picasso",
+        "Leonardo da Vinci",
+        "Michelangelo",
+      ],
+      answer: "Leonardo da Vinci",
+    },
+    {
+      question: "In welchem Jahr fand die erste Mondlandung statt?",
+      bild: "https://cdn.pixabay.com/photo/2018/03/26/14/07/space-3262811_1280.jpg",
+      options: ["1969", "1975", "1981", "1992"],
+      answer: "1969",
+    },
+
+    {
+      question: "wie viele kantone hat die Schweiz?",
+      bild: "https://cdn.pixabay.com/photo/2016/10/21/19/05/switzerland-1758854_1280.png",
+      options: ["23", "24", "25", "26"],
+      answer: "23",
+    },
+    {
+      question: "Was ist die chemische Formel für Wasser?",
+      bild: "https://cdn.pixabay.com/photo/2012/04/26/11/17/periodic-table-42115_1280.png",
+      options: ["H2O", "CO2", "NaCl", "C6H12O6"],
+      answer: "H2O",
+    },
+    {
+      question: "Welcher Planet ist der nächste zur Sonne?",
+      bild: "https://cdn.pixabay.com/photo/2012/05/04/10/17/sun-47083_1280.png",
+      options: ["Venus", "Mars", "Merkur", "Jupiter"],
+      answer: "Merkur",
+    },
+    {
+      question: "Wer hat die Relativitätstheorie entwickelt?",
+      bild: "https://cdn.pixabay.com/photo/2013/07/12/18/31/einstein-153422_1280.png",
+      options: [
+        "Albert Einstein",
+        "Isaac Newton",
+        "Stephen Hawking",
+        "Galileo Galilei",
+      ],
+      answer: "Albert Einstein",
+    },
+
+    {
+      question: "Wie viele Kontinente gibt es auf der Erde?",
+      bild: "https://cdn.pixabay.com/photo/2013/07/12/12/54/world-map-146505_1280.png",
+      options: ["4", "5", "6", "7"],
+      answer: "7",
+    },
+    {
+      question:
+        "Welches ist das älteste noch existierende Weltwunder der Antike?",
+      bild: "https://cdn.pixabay.com/photo/2017/08/13/16/29/eye-of-horus-2637716_1280.png",
+      options: [
+        "Pyramiden von Gizeh",
+        "Hängende Gärten von Babylon",
+        "Zeusstatue des Phidias",
+        "Artemistempel in Ephesos",
+      ],
+      answer: "Pyramiden von Gizeh",
+    },
+    {
+      question: "Welches ist das längste Tier der Welt?",
+      bild: "https://cdn.pixabay.com/photo/2012/04/13/21/31/fish-33712_1280.png",
+      options: ["Blauwal", "Koloss-Kalmar", "Ameise", "Riesenseestern"],
+      answer: "Blauwal",
+    },
+  ];
+  return null;
+};
+
+export default Allgemein;
